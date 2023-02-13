6f045eccf3eb9539ed2b58f3488b23be8a7f4f31
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-13 17:36:24 +0100

    wip

--- src/components/ImageList/ImageList.jsx
+++
@@ -1,9 +0,0 @@
-import { ImageListItem } from "../ImageListItem/ImageListItem";
-import s from "./ImageList.module.css";
-export function ImageList({ imageList }) {
-  return imageList.map((img) => (
-    <ul className={s.cards} key={img.id}>
-      <ImageListItem img={img} />
-    </ul>
-  ));
-}
